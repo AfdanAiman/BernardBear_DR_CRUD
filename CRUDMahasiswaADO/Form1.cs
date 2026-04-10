57dd76b6cf5c20af3c8056d7d64c5dd2f40a48e6
AfdanAiman <afdanaiman06@gmail.com>
2026-04-10 09:43:22 +0700

    Menambahkan koneksi sql server

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -13,10 +13,13 @@ namespace CRUDMahasiswaADO
 {
     public partial class Form1 : Form
     {
+        private readonly SqlConnection conn;
+        private readonly string connectionString = "Data Source=DESKTOP-BQJ792T\\AFDAN;Initial Catalog=DBAkademikADO;Integrated Security=TRUE";
         public Form1()
         {
             InitializeComponent();
         }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
